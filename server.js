3954fe3dd97aaa1bef18be30bd753429883f81ff
Jason Pilz <jasonpilz@gmail.com>
2016-02-27 20:24:27 -0700

    add jade, have 3 passing tests

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const app = express();
+const app     = express();
+const path    = require('path');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
 
+app.use(express.static('static'));
+app.set('view engine', 'jade');
+
 app.get('/', (request, response) => {
-      response.send('Hello World!');
+  response.send(app.locals.title);
 });
 
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '/static/index.html'));
+});
+
+
 if (!module.parent) {
-    app.listen(app.get('port'), () => {
-          console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-    });
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
 }
 
 module.exports = app;
